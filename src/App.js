283f89b68b991ebb4b5bbf6eba830b92ec1b0b4b
Flame Anjal <kinganjalap@gmail.com>
2023-02-10 09:03:50 +0545

    V 0.21

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+   <>
+    <Header title="Nav Bar" searchBar={false}></Header>
+    <br /><br /><br /><br /><br />
+ 
+   </>
+  );
+}
+
+export default App;
